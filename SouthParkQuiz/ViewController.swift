0d87188873352cc1c36dbc85f03a3a878fff6fc7
Anton <verbiants@gmail.com>
2021-04-06 11:50:21 +0300

    Initial Commit

---
+++ SouthParkQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SouthParkQuiz
+//
+//  Created by Антон Вербицкий on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
